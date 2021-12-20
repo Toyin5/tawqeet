4fd2f794a00eb81acc0236e3271db1d023e23089
toyin5 <toyinmuhammed50@gmail.com>
2021-12-20 18:26:32 +0200

    First commit after deploy

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,8 @@
 import "../styles/Footer.css"
-import { Favorite } from "@mui/icons-material"
 function Footer() {
     return (
         <div className="footer">
-            <p>&copy; Mu'aqitah 2021 - All rights reserved - Created with <Favorite /> by <a href="mylink" target="_blank">Ibn Katheer</a></p>
+            <p>&copy; Mu'aqitah 2021 | All rights reserved | Created by <a href="mylink" target="_blank">Ibn Katheer</a></p>
         </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,7 @@
 import "../styles/Header.css"
-import logo from "../logo"
 function Header() {
     return (
         <div>
-            <img src={logo} alt="logo" />
             <span className="header">
                 Muaqitah
             </span>
