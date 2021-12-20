4790a80645a56a5149c66a1c3f16406697e9f635
toyin5 <toyinmuhammed50@gmail.com>
2021-12-20 18:33:13 +0200

    After some changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import "../styles/Footer.css"
 function Footer() {
     return (
         <div className="footer">
-            <p>&copy; Mu'aqitah 2021 | All rights reserved | Created by <a href="mylink" target="_blank">Ibn Katheer</a></p>
+            <p>&copy; Mu'aqitah 2021 | All rights reserved</p>
         </div>
     )
 }
