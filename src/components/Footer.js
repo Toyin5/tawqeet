76d932a3fb926a80cefecfd97c45bb2a95601417
toyin5 <toyinmuhammed50@gmail.com>
2022-01-31 13:36:04 +0200

    Added a link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import "../styles/Footer.css"
 function Footer() {
     return (
         <div className="footer">
-            <p>&copy; Mu'aqitah 2021 | v1.0 | made with &hearts; by <a href="/" target="_blank">Toyin</a></p>
+            <p>&copy; Mu'aqitah 2021 | v1.0 | made with &hearts; by <a href="https://wa.me/message/OBNLXYWLE4HIA1" target="_blank" rel="noreferrer">Toyin</a></p>
         </div>
     )
 }
